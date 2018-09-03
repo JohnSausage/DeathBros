0f8d32dc0360eb18e18a37d5a18ed4afe1e4df22
JulianGreindl <juliangreindl88@gmail.com>
2018-09-03 22:29:28 +0200

    Added TestLevel, GameManager, Pause

---
+++ DeathBros/Assets/Scripts/GameManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : _MB
+{
+    public Level startingLevel;
+
+    public static bool IsPaused { get; private set; }
+
+    public static Player Player { get; private set; }
+    public StateMachine LevelSM { get; private set; }
+
+    public static GameManager Instance { get; private set; }
+
+    public override void Init()
+    {
+        base.Init();
+
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(this);
+        }
+
+        Player = FindObjectOfType<Player>();
+
+        if (!Player)
+            Debug.Log("Player not found");
+
+        LevelSM = new StateMachine();
+        LevelSM.ChangeState(startingLevel);
+    }
+
+    void Update()
+    {
+        LevelSM.Update();
+
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!IsPaused)
+                Pause();
+            else
+                Resume();
+        }
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        IsPaused = true;
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        IsPaused = false;
+    }
+}
---
+++ DeathBros/Assets/Scripts/Level.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour, IState
+{
+    public GameObject levelTransforms;
+
+    public void Enter()
+    {
+        levelTransforms.SetActive(true);
+    }
+
+    public void Execute()
+    {
+    }
+
+    public void Exit()
+    {
+    }
+
+    void Start()
+    {
+        Exit();
+    }
+
+    void Update()
+    {
+
+    }
+}
