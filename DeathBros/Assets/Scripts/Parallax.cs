196f53d26c041c92bea72ff90fa5fa7485364781
JulianGreindl <juliangreindl88@gmail.com>
2018-10-18 08:03:08 +0200

    added parallax

---
+++ DeathBros/Assets/Scripts/Parallax.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    public Transform parallaxCamera;
+    public float parallaxScale = 0.1f;
+
+    public Vector2 start;
+    public Vector2 currentCameraPosition;
+    void Start()
+    {
+        start = parallaxCamera.position;
+    }
+
+    void FixedUpdate()
+    {
+        currentCameraPosition = (Vector2)parallaxCamera.position - start;
+
+        transform.localPosition = -currentCameraPosition * parallaxScale;
+    }
+}
