87a2272a9216071dff75404001ad216535f84a2d
JohnSausage <juliangreindl88@gmail.com>
2018-11-06 19:33:39 +0100

    changed ruins tileset to purple_ruins

---
+++ DeathBros/Assets/TiledMaps/collision_tiles.tsx
@@ -0,0 +1,65 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="collision_tiles" tilewidth="16" tileheight="16" tilecount="256" columns="16">
+ <image source="../Sprites/Tiles/collision_tiles.png" width="256" height="256"/>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="16">
+    <polygon points="0,0 16,-16 16,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="17">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 16,16 0,16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="18">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="16">
+    <polygon points="0,0 16,-8 16,-1 16,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="19">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="8">
+    <polygon points="0,0 16,-8 16,8 0,8"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="20">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 16,8 16,16 0,16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="21">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="8">
+    <polygon points="0,0 16,8 0,8"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="32">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 16,16 16,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="33">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="16">
+    <polygon points="0,0 0,-16 16,-16"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
---
+++ DeathBros/Assets/TiledMaps/purple_ruins_tiles.tsx
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="purple_ruins_tiles" tilewidth="16" tileheight="16" tilecount="256" columns="16">
+ <image source="../Sprites/Tiles/purple_ruins_tiles.png" width="256" height="256"/>
+ <terraintypes>
+  <terrain name="gras_top" tile="0"/>
+  <terrain name="no_gras" tile="0"/>
+ </terraintypes>
+ <tile id="0" terrain=",,,0"/>
+ <tile id="1" terrain=",,0,0"/>
+ <tile id="2" terrain=",,0,"/>
+ <tile id="4" terrain="0,0,0,"/>
+ <tile id="5" terrain="0,0,,0"/>
+ <tile id="16" terrain=",0,,0"/>
+ <tile id="17" terrain="0,0,0,0"/>
+ <tile id="18" terrain="0,,0,"/>
+ <tile id="20" terrain="0,,0,0"/>
+ <tile id="21" terrain=",0,0,0"/>
+ <tile id="32" terrain=",0,,"/>
+ <tile id="33" terrain="0,0,,"/>
+ <tile id="34" terrain="0,,,"/>
+ <tile id="66" terrain=",,,1"/>
+ <tile id="67" terrain=",,1,1"/>
+ <tile id="68" terrain=",,1,"/>
+ <tile id="69" terrain="1,1,1,"/>
+ <tile id="70" terrain="1,1,,1"/>
+ <tile id="82" terrain=",1,,1"/>
+ <tile id="83" terrain="1,1,1,1"/>
+ <tile id="84" terrain="1,,1,"/>
+ <tile id="85" terrain="1,,1,1"/>
+ <tile id="86" terrain=",1,1,1"/>
+ <tile id="98" terrain=",1,,"/>
+ <tile id="99" terrain="1,1,,"/>
+ <tile id="100" terrain="1,,,"/>
+</tileset>
