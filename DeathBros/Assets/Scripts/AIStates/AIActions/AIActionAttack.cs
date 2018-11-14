6f53d2d0fe71f4ad2b544a9656f5e384b2f37198
JohnSausage <juliangreindl88@gmail.com>
2018-11-14 19:02:08 +0100

    worked on bat ai added orange forest as test area

--- DeathBros/Assets/Scripts/AIStates/AIActions/AIActionAttack.cs
+++ DeathBros/Assets/Scripts/AIStates/AIActions/AIActionAttack.cs
@@ -6,9 +6,24 @@ public class AIActionAttack : AIActionSO
     public bool normalAttack = true;
     public bool specialAttack = false;
 
+    [Space]
+
+    public Vector2 targetOffset = Vector2.zero;
+    public bool moveToEnemy = true;
+
+    public override void Enter(AIController aiCtr)
+    {
+        base.Enter(aiCtr);
+
+        aiCtr.Enemy.SetInputs(aiCtr.TargetDirection(targetOffset));
+    }
+
     public override void Execute(AIController aiCtr)
     {
-        aiCtr.Enemy.SetInputs(aiCtr.TargetDirection);
+        if (moveToEnemy)
+            aiCtr.Enemy.SetInputs(aiCtr.TargetDirection(targetOffset));
+        else
+            aiCtr.Enemy.SetInputs(Vector2.zero);
 
         aiCtr.Enemy.Attack = normalAttack;
         aiCtr.Enemy.Special = specialAttack;
