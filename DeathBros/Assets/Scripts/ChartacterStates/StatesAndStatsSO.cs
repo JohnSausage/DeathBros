3c7ce8da9ba3f1d1347e9f3c40ab803cc39f68a0
JohnSausage <juliangreindl88@gmail.com>
2019-03-05 12:57:36 +0100

    started work on nes style

---
+++ DeathBros/Assets/Scripts/ChartacterStates/StatesAndStatsSO.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "StatesAndStats")]
+public class StatesAndStatsSO : ScriptableObject
+{
+    public string idle_anim = "idle";
+    public string walking_anim = "walking";
+    public string running_anim = "running";
+    public string jumpsquat_anim = "jumpsquat";
+    public int jumpsquat_duration = 4;
+
+    public List<CState> GenerateStates()
+    {
+        List<CState> cstates = new List<CState>();
+
+
+
+        return cstates;
+    }
+}
